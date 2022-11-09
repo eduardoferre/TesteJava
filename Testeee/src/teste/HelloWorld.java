bf9842ce63bfe82be25e4b94ed1f55e1bacd16e9
almei <almei@DESKTOP-5QDGFRH>
2022-11-09 00:13:23 -0300

    Primeiro commit

---
+++ Testeee/src/teste/HelloWorld.java
@@ -0,0 +1,37 @@
+package teste;
+
+import java.util.Scanner;
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Scanner scanf = new Scanner(System.in);
+		
+		System.out.println("Hello, World!");
+		
+		int n1;
+		System.out.print("Digite um numero: ");
+		n1 = scanf.nextInt();
+		System.out.println("Numero digitado: "+n1);
+		
+		
+		double a1, a2, a3;
+		System.out.print("Digite a altura do primeiro atleta: ");
+		a1 = scanf.nextDouble();
+		System.out.print("Digite a altura do segundo atleta: ");
+		a2 = scanf.nextDouble();
+		System.out.print("Digite a altura do terceiro atleta: ");
+		a3 = scanf.nextDouble();
+		
+		double media;
+		
+		media = (a1 + a2 + a3) / 3;
+		
+		System.out.println("\nMédia da aultura dos atletas: "+media);
+		
+		System.out.println("Testando commit!");
+		
+		
+		scanf.close();
+	}
+
+}
